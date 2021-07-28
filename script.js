0a047438fc1363ebd9b6ffd64cb720da513c59ce
Denis Lisunov <rev.denisas@gmail.com>
2021-07-28 23:43:52 +0300

    remade style to be less agressive on the eyes and added more script code

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let displayNum1 = 0;
 let num1;
 let num2;
 let displayNum2 = 0;
+let equality = false;
 
 //a function which checks for an operator and applies the correct operator
 
@@ -32,20 +33,26 @@ function prevOp(a,b){
 //from 0 to 100, this is what happens after clicking an operator
 
 function operation(sign){
+    testLength();
     if(prevOperator === undefined){
-        displayNum1 = parseInt(display.textContent);
+        displayNum1 = parseFloat(display.textContent);
         num1 = displayNum1;
         prevOperator = sign;
 
+    }else if(prevOperator == "="){
+        prevOperator = undefined;
+        display.textContent = display.textContent;
+
     }else{
-        displayNum2 = parseInt(display.textContent);
+        displayNum2 = parseFloat(display.textContent);
         num2 = displayNum2;
         currentOperator = sign;
         if(currentOperator == "="){
             num1 = prevOp(num1, num2);
             display.textContent = num1;
             prevOperator = undefined;
-            currentOperator == undefined;
+            currentOperator = undefined;
+            equality = true;
         } else{
             num1 = prevOp(num1, num2);
             prevOperator = currentOperator;
@@ -53,6 +60,12 @@ function operation(sign){
         }
     }
     
+}
+
+function testLength(){
+    if (display.textContent.length > 16){
+        display.textContent = display.textContent.substring(0,16)
+    }
 }
 
 //shorted functions
@@ -82,18 +95,20 @@ function clear() {
     displayNum2 = 0;
     currentOperator = undefined;
     prevOperator = undefined;
+    equality = false;
     num1 = 0;
     num2 = 0;  
 }
 //in case a user wants to write zeros randomly
 
 function check(num){
+    testLength()
     if (display.textContent === "0"){
         display.textContent = `${num}`;
     } else if(displayNum1 != 0){
         display.textContent = `${num}`;
         displayNum1 = 0;
-    } else if(displayNum2 != 0){
+    } else if(displayNum2 != 0 && equality == false){
         display.textContent = `${num}`;
         displayNum2 = 0;
     } else{
@@ -104,7 +119,11 @@ function check(num){
 //puts a dot for decimals 
 
 function dot() {
-    display.textContent += "."
+    if(display.textContent.indexOf(".") == -1){
+        display.textContent += "."
+    } else{
+        display.textContent = display.textContent;
+    }
     }
 
 //selecting buttons and display, adding on click events
